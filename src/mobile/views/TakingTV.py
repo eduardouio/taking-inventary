367b143c92fe9f4a038f3ee178b8f45d122f2d19
Eduardo DELL <eduardouio7@gmail.com>
2023-01-19 16:47:18 -0500

    Listamos solos los productos del reporte

--- src/mobile/views/TakingTV.py
+++ src/mobile/views/TakingTV.py
@@ -5,7 +5,7 @@ from django.views.generic import TemplateView
 from django.http import HttpResponseBadRequest
 
 from products.models import Product
-from sap_migrations.models import SapMigration
+from sap_migrations.models import SapMigration, SapMigrationDetail
 from accounts.models.Team import Team
 from accounts.models.CustomUserModel import CustomUserModel
 from accounts.mixins import ValidateAssistantMixin
@@ -20,13 +20,13 @@ class TakingTV(ValidateAssistantMixin, TemplateView):
     def get(self, request, id_taking, *args, **kwargs):
         context = self.get_context_data(**kwargs)
         user = CustomUserModel.get(request.user)
-        products = serialize('json', Product.objects.all())
+        taking = Taking.get(id_taking)
+        products = serialize('json', self.get_products(taking))
         warenhouses = Warenhouse.objects.all()
         warenhouses = [ {'name': whrs.name, 'pk': whrs.pk} 
             for whrs in warenhouses 
         ]
         my_team = None
-        taking = Taking.get(id_taking)
         for team in taking.teams.all():
             if team.manager == user:
                 my_team = team
@@ -46,16 +46,21 @@ class TakingTV(ValidateAssistantMixin, TemplateView):
             'team': json.dumps(json.loads(my_team)[0]),
             'user': json.dumps(user),
         }
-
         return self.render_to_response({**context, **page_data})
 
-    def post(self, request, id_taking, *args, **kwargs):
-        taking_report = json.loads(request.POST.get('report'))
-        if not taking_report:
-            return HttpResponseBadRequest(
-                'request data is null, please try later'
-            )
-            
-        
+    def get_products(self, taking):
+        found_poducts = []
+        products = json.loads(taking.id_sap_migration.report)['products']
+        for account_code in products:
+            obj_product = Product.get(account_code)
+            if obj_product is None:
+                new_product = SapMigrationDetail.objects.filter(account_code=account_code).first()
+                my_product = Product.objects.create(
+                    account_code=account_code,
+                    name=new_product.name
+                )
+                raise Exception('El prodycto {} no existe en la base'.format(account_code))
+            found_poducts.append(obj_product)
 
-        import ipdb;ipdb.set_trace()
+        return found_poducts
+
