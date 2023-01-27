ac8707c1d9ad8d6cb5a8b765576f5ff3454eb9f8
Eduardo DELL <eduardouio7@gmail.com>
2023-01-27 17:11:58 -0500

    Refactor de migracion

--- src/sap_migrations/views/MigrationListTV.py
+++ src/sap_migrations/views/MigrationListTV.py
@@ -1,37 +1,50 @@
+from time import time
+
 from django.views.generic import TemplateView
 from sap_migrations.models import SapMigration
+from takings.models import Taking
+from django.http import HttpResponseRedirect
 from sap_migrations.lib import LoadMigration
 
 
 # /sap/
 class MigrationListTV(TemplateView):
-	template_name = 'sap_migrations/list_migrations.html'
+    template_name = 'sap_migrations/list_migrations.html'
 
-	def get(self, request, *args, **kwargs):
-		context = self.get_context_data(*args, **kwargs)
+    def get(self, request, *args, **kwargs):
+        start_time = time()
+        context = self.get_context_data(*args, **kwargs)
 
-		if request.GET.get('migrate'):
-			load_migration = LoadMigration()
-			load_migration.load()
+        if request.GET.get('migrate'):
+            load_migration = LoadMigration()
+            load_migration.load()
+            return HttpResponseRedirect('/sap/')
 
-		all_migrations = self.get_all_migrations()
-		page_data = {
-			'all_migrations': all_migrations,
-			'module_name': 'Migraciones SAP',
-			'title_page': 'Saldos Importados Sap',
-			'total_records':len(all_migrations),
-		}
-		context = { **context, **page_data}
-		return self.render_to_response(context=context)
 
-	def get_all_migrations(self):
-		all_migrations = [] 
-		migrations = SapMigration.objects.all()
+        all_migrations = self.get_all_migrations()
 
-		for migration in migrations:
-			all_migrations.append({
-				'migration': migration,
-				'user': SapMigration.get_user(migration)
-			})
-		return all_migrations
-	
+        page_data = {
+            'all_migrations': all_migrations,
+            'module_name': 'Migraciones SAP',
+            'title_page': 'Saldos Importados Sap',
+            'total_records':len(all_migrations),
+            'last_migration': all_migrations[0] if all_migrations else None ,
+            'total_time': time() - start_time,
+        }
+        context = { **context, **page_data}
+        return self.render_to_response(context=context)
+
+    def get_all_migrations(self) -> dict:
+        all_migrations = [] 
+        migrations = SapMigration.objects.all()
+        
+        if not migrations:
+            return all_migrations
+
+        for migration in migrations:
+            all_migrations.append({
+                'migration': migration,
+                'user': SapMigration.get_user(migration),
+                'takings': Taking.get_by_sap_migrations(migration.pk),
+            })
+        return all_migrations
