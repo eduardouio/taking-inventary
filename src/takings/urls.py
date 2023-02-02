7ff79560cc7b999714fdd70e5d574a46d933dc2c
Eduardo DELL <eduardouio7@gmail.com>
2023-02-02 10:55:13 -0500

    Urls para productos

--- src/takings/urls.py
+++ src/takings/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 from takings.views import CreateTakingTP, TakingMasterDetailTV, TakingsList, UpdateTakingCV
 
-app_name ='takings'
+app_name = 'takings'
 
 urlpatterns = [
-    path('detail/<int:pk>', TakingMasterDetailTV.as_view(), name='master-detail-taking'),
+    path('',  TakingsList.as_view(), name="list-migrations"),
+    path('detail/<int:pk>', TakingMasterDetailTV.as_view(),
+         name='master-detail-taking'),
     path('add-report/', UpdateTakingCV.as_view(), name="update_report"),
-    path('create/<int:id_sap_migration>', CreateTakingTP.as_view(), name="selected_migration"),
-    path('list/',  TakingsList.as_view(), name="list-migrations"),
+    path('create/<int:id_sap_migration>',
+         CreateTakingTP.as_view(), name="selected_migration"),
 ]
