c6eca45b641da8bbc3f3d60b39be556a4a92328d
Developer <eduardouio7@gmail.com>
2023-02-26 05:57:54 -0500

    habilitamos nuevas funciones de tomas

--- src/sap_migrations/views/MigrationListTV.py
+++ src/sap_migrations/views/MigrationListTV.py
@@ -13,13 +13,17 @@ class MigrationListTV(TemplateView):
 
     def get(self, request, *args, **kwargs):
         start_time = time()
+        message = ''
         context = self.get_context_data(*args, **kwargs)
+        action = request.GET.get('action')
 
-        if request.GET.get('migrate'):
+        if action == 'migrate':
             load_migration = LoadMigration()
             load_migration.load()
-            return HttpResponseRedirect('/sap/')
+            return HttpResponseRedirect('/sap/?action=migrated')
 
+        if action == 'migrated':
+            message = 'Nueva Migracion Completada'
 
         all_migrations = self.get_all_migrations()
 
@@ -27,17 +31,18 @@ class MigrationListTV(TemplateView):
             'all_migrations': all_migrations,
             'module_name': 'Migraciones SAP',
             'title_page': 'Saldos Importados Sap',
-            'total_records':len(all_migrations),
-            'last_migration': all_migrations[0] if all_migrations else None ,
+            'total_records': len(all_migrations),
+            'last_migration': all_migrations[0] if all_migrations else None,
             'total_time': time() - start_time,
+            'message': message,
         }
-        context = { **context, **page_data}
+        context = {**context, **page_data}
         return self.render_to_response(context=context)
 
     def get_all_migrations(self) -> dict:
-        all_migrations = [] 
+        all_migrations = []
         migrations = SapMigration.objects.all()
-        
+
         if not migrations:
             return all_migrations
 
--- src/takings/views/TakingsList.py
+++ src/takings/views/TakingsList.py
@@ -2,7 +2,6 @@ from time import time
 import json
 
 from django.views.generic import ListView
-from django.http import Http404
 
 from accounts.mixins import ValidateManagerMixin
 from takings.models import Taking, TakinDetail
@@ -15,14 +14,31 @@ class TakingsList(ValidateManagerMixin, ListView):
 
     def get(self, request, *args, **kwargs):
         start_time = time()
-        self.object_list = self.get_queryset()
+        message = ''
 
+        if request.GET.get('action') == 'delete':
+            my_taking = Taking.get(request.GET.get('pk'))
+            if my_taking:
+                my_taking_details = TakinDetail.get_by_taking(
+                    request.GET.get('pk')
+                )
+                if not my_taking_details:
+                    message = 'Toma {} eliminada correctamente'.format(
+                        request.GET.get('pk')
+                    )
+                    try:
+                        my_taking.delete()
+                    except:
+                        message = 'No se puede Eliminar el registro, tiene dependencias, si desea eliminar comunicarse con soporte@vinesa.com.ec'
+
+        self.object_list = self.get_queryset()
         extra_context = {
             'module_name': 'Tomas Físicas',
             'title_page': 'Lista de Tomas',
             'total_records': len(self.object_list),
             'total_time': time() - start_time,
             'last_taking': self.object_list[0],
+            'message': message,
         }
         context = self.get_context_data(**kwargs)
         return self.render_to_response({**context, **extra_context})
