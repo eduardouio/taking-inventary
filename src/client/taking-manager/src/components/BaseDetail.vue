374503e4600c547ca80145294cf2a61a1f036101
Eduardo Villota <eduardouio7@gmail.com>
2023-05-17 18:01:57 -0500

    cargamos el loader

--- src/client/taking-manager/src/components/BaseDetail.vue
+++ src/client/taking-manager/src/components/BaseDetail.vue
@@ -1,5 +1,7 @@
 <template>
-    <div class="container-fluid mt-3 border p-3 bg-white">
+    <div>
+    <loader v-if="!item_report"></loader>
+    <div class="container-fluid mt-3 border bg-white" v-if="item_report">
         <div class="row bg  bg-ligth bg-gradient">
             <div class="col-1">
                 <span class="badge bg-dark">
@@ -7,14 +9,15 @@
                 </span>
             </div>
             <div class="col-10 text-center">
-                <h4 class="text-primary" id="app-product-desc">
+                <span class="text-primary" id="app-product-desc">
                     {{ selected_item.product.fields.name }}
                     <br>
                     <small>{{ selected_item.product.fields.ean_13_code }}</small>
-                </h4>
+                </span>
+                <hr>
             </div>
             <div class="col text-end">
-                <button class="btn btn-outline-danger fs-3" @click="showReport">
+                <button class="btn btn-outline-danger fs-5" @click="showReport">
                     <i class="fas fa-close"></i>
                 </button>
             </div>
@@ -65,16 +68,17 @@
                                 <table class="table table-bordered" style="widows: 100%;">
                                     <thead>
                                         <tr>
+                                            <th>#</th>
+                                            <th>Hora</th>
                                             <th scope="col">Grupo</th>
                                             <th scope="col">Cantidad</th>
                                             <th scope="col">Diferencia</th>
+                                            <th><i class="fas fa-cogs"></i></th>
                                         </tr>
                                         </thead>
                                         <tbody>
-                                            <tr>
-                                                <td>Grupo 1</td>
-                                                <td>10</td>
-                                                <td>0</td>
+                                            <tr v-for="(item, index) in item_report.query" :key="item.query">
+                                                  <td class="text-center">{{ index +1 }}</td>
                                             </tr>
                                         </tbody>
                                 </table>
@@ -91,13 +95,24 @@
             </div>
         </div>
     </div>
+    </div>
 </template>
-
 <script>
+import Loader from './Loader.vue';
+
 export default {
+  components: { Loader },
     //Mostamos la informacion adicional usando una tabla maestro detalle en la que se muestra la informacion de los grupos y los productos que se han tomado
     name: 'BaseDetail',
+    componets:{
+        Loader,
+    },
     emits: ['showReport'],
+    data(){
+        return{
+            item_report:null,
+        }
+    },
     props: {
         selected_item: {
             type: Object,
@@ -111,16 +126,40 @@ export default {
             type: String,
             required: true,
         },
-    }, methods: {
+        report:{
+            type:Object,
+            required:true,
+        }
+    },methods: {
         showReport() {
             this.$emit('showReport');
         },
+        loadDetailData(){
+            console.log('obtenemos el detalle del producto');
+            let url = '/takings/api/taking-detail/taking/{pk_taking}/product/{pk_product}/'.replace(
+                '{pk_product}', this.selected_item.product.fields.account_code
+            ).replace(
+                '{pk_taking}', this.report.report.taking.pk
+            )
+            const xhr_data = new XMLHttpRequest();
+            xhr_data.open('GET', this.base_url + url);
+            xhr_data.setRequestHeader('Content-Type', 'application/json');
+            xhr_data.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+            xhr_data.onload = () =>{
+                this.item_report = JSON.parse(xhr_data.responseText);
+            };
+            xhr_data.send();
+
+        },
+    },
+    mounted(){
+        this.loadDetailData();
     },
     computed: {
         // si el item no tiene imagen mostramos imagen por defecto
         image_url() {
             if (this.selected_item.product.fields.image_front) {
-                return this.base_url + '/media/' + this.selected_item.product.fields.image_front;
+                return this.base_url + '/media' + this.selected_item.product.fields.image_front;
             } else {
                 return this.base_url + '/static/img/generic_product.png';
             }
