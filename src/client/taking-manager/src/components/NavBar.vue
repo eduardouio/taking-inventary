60b7a5e93b2ac3404243981f51c2bd39d51671f9
Eduardo Villota <eduardouio7@gmail.com>
2023-05-28 06:47:45 -0500

    terminamos la barra de informacion toma

--- src/client/taking-manager/src/components/NavBar.vue
+++ src/client/taking-manager/src/components/NavBar.vue
@@ -2,7 +2,7 @@
   <div class="bg-light">
     <nav class="navbar navbar-expand-lg navbar-light">
       <div class="container-fluid">
-        <a class="navbar-brand" href="#">TOMA # <span v-if="report.report" v-text="report.report.taking.pk"></span></a>
+        <a class="navbar-brand" href="#">TOMA # <span v-if="report" v-text="report.taking.id_taking"></span></a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText"
           aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
@@ -10,18 +10,29 @@
         <div class="collapse navbar-collapse" id="navbarText">
           <ul class="navbar-nav me-auto mb-2 mb-lg-0">
             <li class="nav-item">
-                <a href="/" class="nav-link">
-                  Inicio
-                </a>
+              <a href="/" class="nav-link">
+                Inicio
+              </a>
             </li>
-            <li class="nav-item">
-              <span class="nav-link">Empresas &nbsp;<span class="badge rounded-pill bg-info text-dark"> {{ total_enterprises  }}</span></span>
+            <li class="nav-item" @click="showDetail('enterprises')">
+              <span class="nav-link">
+                Empresas &nbsp;
+                <span class="badge rounded-pill bg-info text-dark" v-if="report.enterprises.length"> {{
+                  report.enterprises.length }}</span>
+                <span class="badge rounded-pill bg-info text-dark" v-else> Error </span>
+              </span>
             </li>
-            <li class="nav-item">
-              <a class="nav-link">Bodegas &nbsp;<span class="badge rounded-pill bg-info text-dark"> {{total_warenhouses}}</span></a>
+            <li class="nav-item" @click="showDetail('warenhouses')">
+              <span class="nav-link">Bodegas &nbsp;
+                <span class="badge rounded-pill bg-info text-dark" v-if="warenhouses.length"> {{ warenhouses.length
+                }}</span>
+              </span>
             </li>
-            <li class="nav-item">
-              <a class="nav-link">Usuarios &nbsp;<span class="badge rounded-pill bg-info text-dark"> {{total_users}}</span></a>
+            <li class="nav-item" @click="showDetail('groups')">
+              <span class="nav-link">Grupos &nbsp;
+                <span class="badge rounded-pill bg-info text-dark" v-if="report.teams.length"> {{ report.teams.length
+                }}</span>
+              </span>
             </li>
           </ul>
           <ul class="navbar-nav ml-auto">
@@ -29,22 +40,22 @@
               <span class="nav-link"><span class="text-white">
                   Pendientes:
                 </span>
-                <strong class="badge bg-light text-dark">{{ left_over }}</strong></span>
-            </li>
-            <li class="nav-item border rounded bg-secondary">
-              <span class="nav-link"><span class="text-white">
-                  Faltantes:
-                </span><strong class="badge bg-warning"> {{missing}}</strong></span>
-            </li>
-            <li class="nav-item border rounded bg-secondary">
-              <span class="nav-link"><span class="text-white">
-                  Sobrantes:
-                </span><strong class="badge bg-danger"> {{ earrings}} </strong></span>
+                <strong class="badge bg-danger text-dark">{{ left_over }}</strong></span>
             </li>
             <li class="nav-item border rounded bg-secondary">
               <span class="nav-link"><span class="text-white">
                   Completos:
-                </span><strong class="badge bg-success"> {{ full }} </strong></span>
+                </span><strong class="badge bg-success"> {{ full }} / {{ report.report.length }} </strong></span>
+            </li>
+            <li class="nav-item border rounded bg-secondary">
+              <span class="nav-link">
+              <span class="text-white">
+                Progreso:
+              </span>
+                <strong class="text-light badge bg-primary">
+                  {{ percent_progress }}%
+                </strong>
+              </span>
             </li>
           </ul>
           <span class="nav-div">&nbsp;&nbsp;&nbsp;&nbsp;</span>
@@ -53,43 +64,156 @@
               <i class="fas fa-user text-secondary"></i>
               &nbsp;
               <span class="navbar-text">
-                ISAAC MALAN
+                {{ userdata.first_name }} {{ userdata.last_name }}
                 &nbsp;
-                <i class="fas fa-power-off text-danger"></i>
+                <a href="/accounts/logout/">
+                  <i class="fas fa-power-off text-danger"></i>
+                </a>
               </span>
             </li>
           </ul>
         </div>
       </div>
     </nav>
+    <div class="container-fluid">
+      <!--Vista Empresas-->
+      <div class="row" v-if="show_detail.enterprises">
+        <div class="col">
+          <div class="card" style="width: 30rem;">
+             <div class="card-header">
+               <h5 class="card-title text-info">Empresas</h5>
+            </div>
+            <div class="card-body">
+              <h6 class="card-subtitle mb-2 text-muted">Listado de Propietarios</h6>
+              <ul class="list-group">
+                <li v-for="(enterprise, idx) in report.enterprises" :key="enterprise" class="list-group-item">
+                  <span class="badge bg-secondary">{{ idx + 1 }}</span> {{ enterprise }}
+                </li>
+              </ul>
+              <button class="btn btn-sm btn-outline-dark mt-2" @click="showDetail">
+                  <i class="fas fa-xmark"></i> Cerrar
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!--/Vista Empresas-->
+      <!--Vista Bodegas-->
+      <div class="row" v-if="show_detail.warenhouses">
+        <div class="col">
+          <div class="card" style="width: 30rem;">
+            <div class="card-header">
+              <h5 class="card-title text-info">Bodegas</h5>
+            </div>
+            <div class="card-body">
+              <h6 class="card-subtitle mb-2 text-muted">Listado de Bodegas</h6>
+              <ul class="list-group">
+                <li v-for="(warenhouse, idx) in warenhouses" :key="warenhouse" class="list-group-item">
+                  <span class="badge bg-secondary">{{ idx + 1 }}</span> {{ warenhouse }}
+                </li>
+              </ul>
+              <button class="btn btn-sm btn-outline-dark mt-2" @click="showDetail">
+                    <i class="fas fa-xmark"></i> Cerrar
+                </button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!--/Vista Bodegas-->
+      <!--Vista Grupos-->
+      <div class="row" v-if="show_detail.groups">
+        <div class="col">
+          <div class="card" style="width: 100rem;">
+            <div class="card-header">
+              <h5 class="card-title text-info">Grupos</h5>
+            </div>
+            <div class="card-body">
+              <h6 class="card-subtitle mb-2 text-muted">Listado de Grupos</h6>
+              <table class="table table-hover table-bordered">
+                <thead>
+                  <tr class="text-center">
+                    <th>Grupo</th>
+                    <th>Manager</th>
+                    <th>Asistente</th>
+                    <th><i class="fas fa-refresh"></i></th>
+                    <th>Ultima</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr v-for="team in report.teams" :key="team">
+                     <td class="text-center">{{ team.group_number }}</td>
+                     <td>{{ team.manager.first_name }} {{ team.manager.last_name }}</td>
+                     <td>{{ team.warenhouse_assistant }}</td>
+                     <td class="text-center">{{ team.activity.count }}</td>
+                     <td>
+                      <span v-if="team.activity.last_taking">
+                        {{ new Date(team.activity.last_taking).toLocaleString('es-Ec') }}
+                      </span>
+                      <span v-else class="text-danger">No tiene</span>
+                      </td>
+                  </tr>
+                </tbody>
+              </table>
+              <button class="btn btn-sm btn-outline-dark mt-2" @click="showDetail">
+                      <i class="fas fa-xmark"></i> Cerrar
+                  </button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!--/Vista Grupos-->
+    </div>
   </div>
 </template>
 <script>
 export default {
   name: 'NavBar',
+  data() {
+    return {
+      show_detail: {
+        enterprises: false,
+        warenhouses: false,
+        groups: false,
+      }
+    }
+  },
   props: {
     report: {
       type: Object,
       required: true
+    }, warenhouses: {
+      type: Array,
+      required: true
+    }, userdata: {
+      type: Object,
+      required: true
     }
-  },computed:{
-    total_enterprises(){
-      return 0;
-    },total_warenhouses(){ 
-      return 0;
-    },total_users(){
-      return 0;
+  }, computed: {
+    // items completos
+    full() {
+      return this.report.report.filter(
+        item => item.is_complete == true
+      ).length;
+    }, left_over() {
+      return this.report.report.filter(
+        item => item.is_complete == false
+      ).length;
+    },percent_progress() {
+      return Math.round((this.full / this.report.report.length) * 100);
+    }
+  },methods:{
+    showDetail(name=null){
+      this.show_detail = {
+        enterprises: false,
+        warenhouses: false,
+        groups: false,
+      };
+      if (name === null){
+        return
+      }
+      this.show_detail[name]=true;
     },
-    full(){
-      return 0;
-    },left_over(){
-      return 0;
-    },missing(){
-      return 0;
-    },earrings(){
-      return 0;
-    },
-  },
+  }
 }
 </script>
 
