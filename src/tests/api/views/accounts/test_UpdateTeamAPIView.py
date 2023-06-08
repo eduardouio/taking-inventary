3dc30211969b384841531f90890c1be119057666
Eduardo Villota <eduardouio7@gmail.com>
2023-06-08 16:49:08 -0500

    correccion de test

--- src/tests/api/views/accounts/test_UpdateTeamAPIView.py
+++ src/tests/api/views/accounts/test_UpdateTeamAPIView.py
@@ -1,15 +1,31 @@
 import pytest
 from django.urls import reverse
 from rest_framework.test import APIClient
-
-from api.views.accounts import UpdateTeamAPIView
+from accounts.models.Team import Team
 
 
 @pytest.mark.django_db
-class TestUpdateTeamAPIView(APIClient):
+class TestUpdateTeamAPIView():
 
-    def test_update_team(self):
+    def setup_method(self):
+        self.client = APIClient()
+        # recuperamso equipo original
+        self.team_data = Team.objects.get(pk=1)
+
+    def test_update_team_success(self):
+
+        # datos a modificar
+        my_updated_team = {
+            'id_team': 1,
+            'id_taking': 1,
+            'manager': self.team_data.manager.id,
+            'notes': 'Camabiamos el nombre del equipo',
+        }
+
         url = reverse('update-team', kwargs={'id_team': 1})
-        response = self.put(url, data={'name': 'test'})
+        response = self.client.put(url, data=my_updated_team)
         assert response.status_code == 200
-        assert response.data['name'] == 'test'
+
+        # verificamos los cambios
+        updated_data = Team.objects.get(pk=1)
+        assert updated_data.notes == my_updated_team['notes']
