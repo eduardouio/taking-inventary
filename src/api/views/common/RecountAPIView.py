4b5f006a0cb6d926e9a2e42cc7efea4e48309713
Eduardo Villota <eduardouio7@gmail.com>
2023-06-09 05:37:53 -0500

    refactor

--- src/api/views/common/RecountAPIView.py
+++ src/api/views/common/RecountAPIView.py
@@ -5,6 +5,15 @@ from recounts.lib import MakeRecount
 
 # /api/common/recount/<int:id_taking>/<str:account_code>/
 class RecountAPIView(APIView):
+    """ Elimna las tomas de inventario que no esten cerradas
+        elimnar todas las tomas de inventario que no esten cerradas
+
+        /api/common/recount/<int:id_taking>/all/
+
+        eliminar las tomas de un item
+        /api/common/recount/<int:id_taking>/<str:account_code>/
+    """
+
     def get(self, request, id_taking, account_code, format=None):
         account_code = account_code if account_code != 'all' else None
 
