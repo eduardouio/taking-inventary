82195db70a916e105cd64cd44ad5b09fb9e6145e
Eduardo Villota <eduardouio7@gmail.com>
2023-06-12 14:27:49 -0500

    rfactor

--- src/common/SAPMigrationConnector.py
+++ src/common/SAPMigrationConnector.py
@@ -3,27 +3,33 @@ from decimal import Decimal
 
 from config.secrets_config import SAP_CONNECTION, MIGRATION_QUERY
 
+
 class SAPMigrationConnector(object):
+    
+    def __init__(self):
+        try:
+            self.conn = connect(**SAP_CONNECTION, login_timeout=5)
+        except:
+            self.conn = False
 
-	def __init__(self):
-		try:
-			self.conn = connect(**SAP_CONNECTION, login_timeout=5)
-		except:
-			self.conn = False
+    def runMigration(self) -> list:
+        """Ejecuta la consulta el servidor y retorna un arreglo"""
+        if self.conn == False:
+            return False
 
-	def runMigration(self):
-		if self.conn == False:
-			return False
+        # ejecuramos la consulta en SAP
+        cursor = self.conn.cursor(as_dict=True)
+        cursor.execute(MIGRATION_QUERY)
+        report = [i for i in cursor]
 
-		cursor = self.conn.cursor(as_dict=True)
-		cursor.execute(MIGRATION_QUERY)
-		report = [i for i in cursor] 
-		for item in report:
-			for k,v in zip(item.keys(),item.values()):
-				if isinstance(v, Decimal):
-					item[k] = int(v)
-			
-		cursor.close()
-		self.conn.close()
-		return report
-		
+        # generamos el arreglo a retorar
+        for item in report:
+            for k, v in zip(item.keys(), item.values()):
+                if isinstance(v, Decimal):
+                    item[k] = int(v)
+
+        # cerramos la conexion
+        cursor.close()
+        self.conn.close()
+        # retornamos el reporte
+        return report
