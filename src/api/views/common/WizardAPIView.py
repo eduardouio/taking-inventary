7bd1ba39d9a6237bffdfa6c167d779d1690fd880
Eduardo Villota <eduardouio7@gmail.com>
2023-06-20 15:25:39 -0500

    refactor

--- src/api/views/common/WizardAPIView.py
+++ src/api/views/common/WizardAPIView.py
@@ -1,20 +1,26 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from sap_migrations.models import SapMigration
-from api.Serializers import SapMigrationSerializer
+from api.Serializers import SapMigrationSerializer, CustomUserSerializer
+from common.WizardMigrationData import WizardMigrationData
 
 
-# /api/common/taking-wizard/
+# /api/common/wizard-assistant/<int:id_sap_migration>/
 class WizardAPIView(APIView):
 
     def get(self, request, id_sap_migration, *args, **kwargs):
-        sap_migration = SapMigration.objects.get(pk=id_sap_migration)
+        try:
+            report = WizardMigrationData().get(id_sap_migration)
+        except Exception as e:
+            return Response({'detail': str(e)}, status=404)
+
+        sap_migration = SapMigrationSerializer(report['sap_migration']).data
+        all_users = CustomUserSerializer(report['all_users'], many=True).data
 
         wizard_data = {
-            'sap_migration': SapMigrationSerializer(sap_migration).data,
-            'warenhouses': [],
-            'type_products': [],
-            'all_users': [],
+            'sap_migration': sap_migration,
+            'warenhouses': report['warenhouses'],
+            'type_products': report['type_products'],
+            'all_users': all_users,
         }
 
         return Response(wizard_data)
