8362713996c29d49093d372951a22a55d130cd8d
Eduardo Villota <eduardouio7@gmail.com>
2023-08-10 18:38:11 -0500

    refactor test de reconteo

---
+++ src/tests/recounts/test_MakeRecount.py
@@ -0,0 +1,10 @@
+import pytest
+
+from takings.models import Taking
+from recounts.models import RecountDetails, RecountTakings
+
+
+class TestMakeRecount:
+
+    def tet_make_recount(self):
+        pass
