fc895c2d7e81b8ea64407f118b26c2c421088cb9
Eduardo Villota <eduardouio7@gmail.com>
2023-08-12 12:51:16 -0500

    mostrar errores en loader y ocultar en peticiones

--- src/client/taking-manager/src/components/Loader.vue
+++ src/client/taking-manager/src/components/Loader.vue
@@ -1,31 +1,43 @@
 <template>
     <div class="container mt-5">
-        <div class="row">
+        <div class="row" v-if="serverStatus.fetching">
             <div class="col text-center">
             <div class="spinner-border text-primary" style="width: 20rem; height: 20rem;" role="status">
             </div>
         </div>
         </div>
-        <div class="row mt-5">
+        <div class="row mt-5" v-if="!serverStatus.error">
             <div class="col text-center">
                     <h1 class="text-secondary text-blink">
                         Cargando Datos...
                     </h1>
             </div>
         </div>
+        <div class="row mt-5 p-5" v-if="serverStatus.error">
+            <div class="col text-center">
+                    <h1 class="text-danger">
+                        {{ serverStatus.message  }}
+                    </h1>
+            </div>
     </div>
+</div>
 </template>
-
 <script>
 export default {
-    name: 'Loader'
+    name: 'Loader',
+    props: {
+        serverStatus: {
+            type: Object,
+            required: true,
+        },
+    },
 }
 </script>
 
 <style scoped>
     @keyframes blinker {
         50% {
-            opacity: 20%;
+            opacity: 40%;
             color:red;
         }
 
