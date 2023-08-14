a7560cd44772ee596d43dcf167c36d6747744c86
Eduardo Villota <eduardouio7@gmail.com>
2023-08-14 12:03:04 -0500

    mostramos y coltamos secciones

--- src/client/taking-manager/src/components/NavBar.vue
+++ src/client/taking-manager/src/components/NavBar.vue
@@ -2,9 +2,10 @@
   <div class="bg-light">
     <nav class="navbar navbar-expand-lg navbar-light">
       <div class="container-fluid">
-        <a class="navbar-brand" href="#"> 
-           [T-<span v-if="report" v-text="report.taking.id_taking"></span>]
-          {{ report.taking.name}}</a>
+        <a class="navbar-brand text-primary" href="#"> 
+            [T-<span v-if="taking" v-text="taking.id_taking"></span>]
+            {{ taking.name}}
+        </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText"
           aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
@@ -35,43 +36,48 @@
             <li class="nav-item" @click="showDetail('enterprises')">
               <span class="nav-link">
                 Empresas &nbsp;
-                <span class="badge rounded-pill bg-info text-dark" v-if="report.enterprises.length"> {{
-                  report.enterprises.length }}</span>
+                <span class="badge rounded-pill bg-info text-dark" v-if="allEnterprises"> {{
+                  allEnterprises.length }}</span>
                 <span class="badge rounded-pill bg-info text-dark" v-else> Error </span>
               </span>
             </li>
             <li class="nav-item" @click="showDetail('warenhouses')">
               <span class="nav-link">Bodegas &nbsp;
-                <span class="badge rounded-pill bg-info text-dark" v-if="warenhouses.length"> {{ warenhouses.length
+                <span class="badge rounded-pill bg-info text-dark" v-if="allWarenhouses.length"> {{ allWarenhouses.length
                 }}</span>
               </span>
             </li>
             <li class="nav-item" @click="showDetail('groups')">
               <span class="nav-link">Grupos &nbsp;
-                <span class="badge rounded-pill bg-info text-dark" v-if="report.teams.length"> {{ report.teams.length
-                }}</span>
+                <span class="badge rounded-pill bg-info text-dark" v-if="allUsersAssistants.length">
+                {{ allUsersAssistants.length}}
+                </span>
               </span>
             </li>
           </ul>
           <ul class="navbar-nav ml-auto">
             <li class="nav-item border">
               <span class="nav-link"><span>
+                <small>
                   Pendientes:
+                </small>
                 </span>
                 <strong class="badge bg-danger text-dark">{{ left_over }}</strong></span>
             </li>
             &nbsp;
             <li class="nav-item border">
               <span class="nav-link"><span>
+                <small>
                   Completos:
-                </span><strong class="badge bg-success"> {{ full }} / {{ report.report.length }} </strong></span>
+                </small>
+                </span><strong class="badge bg-success"> {{ full }} / {{ reportTaking.length }} </strong></span>
             </li>
               &nbsp;
             <li class="nav-item border">
               <span class="nav-link">
-                <span>
+                <small>
                   Progreso:
-                </span>
+                </small>
                 <strong class="text-light badge bg-primary">
                   {{ percent_progress }}%
                 </strong>
@@ -84,7 +90,9 @@
               <i class="fas fa-user text-secondary"></i>
               &nbsp;
               <span class="navbar-text">
-                {{ userdata.first_name }} {{ userdata.last_name }}
+                <small class="bnavbar-brand">
+                  {{ userManager.username }}
+                </small>
                 &nbsp;
                 <a href="/accounts/logout/">
                   <i class="fas fa-power-off text-danger"></i>
@@ -97,16 +105,16 @@
     </nav>
     <div class="container-fluid">
       <!--Vista Empresas-->
-      <div class="row" v-if="show_detail.enterprises">
+      <div class="row" v-if="showSection.enterprises" >
         <div class="col-4">
           <div class="card">
             <div class="card-header">
               <h5 class="card-title text-info">Empresas</h5>
             </div>
             <div class="card-body">
-              <h6 class="card-subtitle mb-2 text-muted">Listado de Propietarios</h6>
+              <h6 class="card-subtitle mb-1 text-muted">Listado de Propietarios</h6>
               <ul class="list-group">
-                <li v-for="(enterprise, idx) in report.enterprises" :key="enterprise" class="list-group-item">
+                <li v-for="(enterprise, idx) in allEnterprises" :key="enterprise" class="list-group-item">
                   <span class="badge bg-secondary">{{ idx + 1 }}</span> {{ enterprise }}
                 </li>
               </ul>
@@ -118,8 +126,8 @@
         </div>
       </div>
       <!--/Vista Empresas-->
-      <!--Vista Bodegas-->
-      <div class="row" v-if="show_detail.warenhouses">
+       <!--Vista Bodegas-->
+       <div class="row" v-if="showSection.warenhouses">
         <div class="col-4">
           <div class="card">
             <div class="card-header">
@@ -132,7 +140,7 @@
               <h6 class="card-subtitle mb-2 text-muted">Listado de Bodegas</h6>
               <ul class="list-group">
                 <li 
-                    v-for="(warenhouse, idx) in warenhouses" 
+                    v-for="(warenhouse, idx) in allWarenhouses" 
                     :key="warenhouse" 
                     class="list-group-item" 
                     @click="warenhouse.selected = !warenhouse.selected">
@@ -180,7 +188,7 @@
                 </thead>
                 <tbody>
                   <tr 
-                    v-for="(item, idx) in report.all_warenhouses" 
+                    v-for="(item, idx) in reportTaking.all_warenhouses" 
                     :key="item"
                     @click="item.selected = !item.selected"
                     >
@@ -205,160 +213,92 @@
         </div>
       </div>
       <!--/Vista Bodegas-->
-      <!--Vista Grupos-->
-      <div class="row" v-if="show_detail.groups">
-        <div class="col-8">
-          <div class="card">
-            <div class="card-header">
-              <h5 class="card-title text-info">
-                <i class="fa-solid fa-users"></i>
-                Grupos
-              </h5>
-            </div>
-            <div class="card-body">
-              <h6 class="card-subtitle mb-2 text-muted">Listado de Grupos</h6>
-              <table class="table table-hover table-bordered">
-                <thead>
-                  <tr class="text-center">
-                    <th>Grupo</th>
-                    <th>Manager</th>
-                    <th>Asistente</th>
-                    <th><i class="fas fa-refresh"></i></th>
-                    <th>Ultima</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr v-for="team in report.teams" :key="team">
-                    <td class="text-center">{{ team.group_number }}</td>
-                    <td>{{ team.manager.first_name }} {{ team.manager.last_name }}</td>
-                    <td>{{ team.warenhouse_assistant }}</td>
-                    <td class="text-center">{{ team.activity.count }}</td>
-                    <td>
-                      <span v-if="team.activity.last_taking">
-                        {{ new Date(team.activity.last_taking).toLocaleString('es-Ec') }}
-                      </span>
-                      <span v-else class="text-danger">No tiene</span>
-                    </td>
-                  </tr>
-                </tbody>
-              </table>
-              <button class="btn btn-sm btn-outline-dark mt-2" @click="showDetail">
-                <i class="fas fa-xmark"></i> Cerrar
-              </button>
-              &nbsp;
-              <button class="btn btn-sm btn-outline-primary mt-2">
-                <i class="fas fa-user-plus"></i>
-                Agregar Grupo
-              </button>
-            </div>
-          </div>
-        </div>
-        <div class="col-4">
-            <div class="card">
-              <div class="card-header">
-                <h5 class="card-title text-info">
-                  <i class="fa-solid fa-user-plus"></i>
-                  &nbsp;
-                  Agregar Grupos
-                  <button class="btn btn-outline-success btn-sm float-end" @click="addTeams">
-                    <i class="fa-solid fa-check"></i>
-                    Aplicar Cambios
-                  </button>
-                </h5>
-              </div>
-              <div class="card-body">
-                  <input type="text" v-model="my_query" placeholder="Buscar" @keyup="filterUsers" class="float-end form-control form-control-sm ">
-                  <table class="table table-hover table-bordered">
-                    <thead>
-                      <tr>
-                        <th>#</th>
-                        <th>Asistente</th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      <tr v-for="(user,idx) in all_users_assistants" :key="user.id" @click="user.selected = !user.selected">
-                        <td class="text-center">{{ idx+1 }}</td>
-                        <td>
-                          <span v-if="user.selected" class="badge bg-success">
-                            &nbsp; Marcado Para Agregar
-                          </span>
-                          {{ user.first_name }} {{ user.last_name }}
-                        </td>
-                      </tr>
-                    </tbody>
-                  </table>
-              </div>
-            </div>
-          </div>
-      </div>
-      <!--/Vista Grupos-->
+
     </div>
   </div>
 </template>
 <script>
+import { all } from 'axios';
+
 export default {
   name: 'NavBar',
   emits: ['updateWarenhouses', 'updateGroups'],
   data() {
     return {
-        show_detail: {
-        enterprises: false,
-        warenhouses: false,
-        groups: false,
+        showSection: {
+          enterprises: true,
+          warenhouses: false,
+          groups: false,
         },
       my_query:null,
-      all_users_assistants: null
+      allUsersAssistants: null
     }
   },
   props: {
-    report: {
+    reportTaking: {
       type: Object,
       required: true
-    }, warenhouses: {
+    }, allWarenhouses: {
       type: Array,
       required: true
-    }, userdata: {
+    }, confData: {
       type: Object,
       required: true
-    },
-    base_url: {
-      type: String,
+    },taking: {
+      type: Object,
       required: true
-    },csrf_token: {
-      type: String,
+    },userManager: {
+      type: Object,
+      required: true
+    },allUsersAssistants: {
+      type: Array,
+      required: true
+    },reportTakingTaking: {
+      type: Object,
+      required: true
+    },allEnterprises: {
+      type: Array,
       required: true
     }
   }
   ,computed: {
     // items completos
     full() {
-      return this.report.report.filter(
+      return this.reportTaking.filter(
         item => item.is_complete == true
       ).length;
     }, left_over() {
-      return this.report.report.filter(
+      return this.reportTaking.filter(
         item => item.is_complete == false
       ).length;
     }, percent_progress() {
-      return Math.round((this.full / this.report.report.length) * 100);
+      return Math.round((this.full / this.reportTaking.length) * 100);
     }
   }, methods: {
     // Mostramos u ocultamos las vistas de empresas, bodegas y grupos
     showDetail(name = null) {
-      this.show_detail = {
+      // si la seccion esta abierta la cerramos
+      if (this.showSection[name] == true){
+        this.showSection[name] = false;
+        return
+      }
+      // cerramos todas las secciones
+      this.showSection = {
         enterprises: false,
         warenhouses: false,
         groups: false,
       };
+      // si no se envia un nombre de seccion, no hacemos nada
       if (name === null) {
         return
       }
-      this.show_detail[name] = true;
+      // abrimos la seccion que se envia
+      this.showSection[name] = true;
+      return
     },
     // filtramos usuarios del cuadro
     filterUsers() {
-      this.all_users_assistants = this.report.all_users_assistants.map(item=>item);
-      this.all_users_assistants = this.all_users_assistants.filter(
+      this.allUsersAssistants = this.allUsersAssistants.filter(
         user => {
           return user.first_name.toLowerCase().includes(this.my_query.toLowerCase()) ||
             user.last_name.toLowerCase().includes(this.my_query.toLowerCase())
@@ -372,13 +312,13 @@ export default {
   // agregamos los grupos
   addTeams() {
     const teams = {
-      'teams': this.all_users_assistants.filter(item => item.selected)
+      'teams': this.allUsersAssistants.filter(item => item.selected)
     } ;
     
     let xhr_team = new XMLHttpRequest();
     xhr_team.open(
       'POST', 
-      this.base_url + '/api/common/add-team-taking/' + this.report.taking.id_taking + '/'
+      this.base_url + '/api/common/add-team-taking/' + this.reportTaking.taking.id_taking + '/'
       );
     xhr_team.setRequestHeader('Content-Type', 'application/json');
     xhr_team.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
@@ -393,9 +333,6 @@ export default {
       alert('Error al agregar los grupos');
     }
   },//prox method
-
-  },mounted(){
-    this.all_users_assistants = this.report.all_users_assistants.map(item=>item);
   }
 }
 </script>
