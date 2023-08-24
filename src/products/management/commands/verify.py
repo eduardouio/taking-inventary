26e4b7f2cb668a9336c0ca082b90b1f156ed59f3
Eduardo Villota <eduardouio7@gmail.com>
2023-08-23 21:24:00 -0500

    mostramos el verify

--- src/products/management/commands/verify.py
+++ src/products/management/commands/verify.py
@@ -12,21 +12,18 @@ class Command(BaseCommand):
     
         for product in queryset:
             if product.image_front:
-                if not product.image_front.url.startswith('/media/app/products/images/'):
-                    errors.append(product.account_code)
-                    print('Producto:{} -> {} no tiene imagen {} '.format(
-                        product.account_code,
-                        product.name, product.image_front.url
-                    ))
-        
-                if not product.image_front.url.endswith('.jpg'):
+                if not product.image_front.url.startswith('/media/app/images/products/'):
                     errors.append(product.account_code)
                     print('Producto:{} -> {} no tiene imagen {} '.format(
                         product.account_code,
                         product.name, product.image_front.url
                     ))
             else:
-                print(product.account_code)
+                self.stdout.write(
+                    self.style.WARNING('El producto {} -> {} no tiene imagen'.format(
+                        product.account_code, product.name
+                    ))
+                )    
         self.stdout.write(
                 self.style.SUCCESS('Terminamos la revision')
             )
