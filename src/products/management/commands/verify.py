a4f380cead40ecfe2e98f0b11e44c23ad7b4d5c7
Eduardo Villota <eduardouio7@gmail.com>
2023-08-24 12:37:19 -0500

    verificador de imagenes

--- src/products/management/commands/verify.py
+++ src/products/management/commands/verify.py
@@ -9,35 +9,24 @@ class Command(BaseCommand):
     def handle(self, *args, **kwargs):
         queryset = Product.objects.all()
         errors = []
-    
+
+        print('Verificando urls de imagenes')
         for product in queryset:
             if product.image_front:
                 if not product.image_front.url.startswith('/media/app/images/products/'):
-                    import ipdb; ipdb.set_trace()
-                    errors.append(product.account_code)
                     print('Corrigiendo Url')
-                    print(product.image_front.url)
-                    print(product.image_front.url.replace(
+                    old_url = product.image_front.url.replace(
                         '/app/products/images/', '/app/images/products/'
-                    ))
-                    product.image_front = product.image_front.url.replace(
-                        '/app/products/images/', '/app/images/products/'
-                    ).split('/')[:]
+                    ).split('/')
+                    index = old_url.index('app')
+                    product.image_front = '/'.join(old_url[index:])
                     product.save()
-                    print('Url corregida')
-                    print('Producto:{} -> {} no tiene imagen {} '.format(
-                        product.account_code,
-                        product.name, product.image_front.url
-                    ))
-               
+                    print('Url corregida {}'.format(product.name))
 
-            #else:
-            #    self.stdout.write(
-            #        self.style.ERROR('El producto {} -> {} no tiene imagen'.format(
-            #            product.account_code, product.name
-            #        ))
-            #    )    
-        self.stdout.write(
-                self.style.SUCCESS('Terminamos la revision')
-            )
+            else:
+                errors.append(product)
+
+        print('Productos sin imagen')
+        for product in errors:
+            print('{}->{}'.format(product.account_code, product.name))           
               
