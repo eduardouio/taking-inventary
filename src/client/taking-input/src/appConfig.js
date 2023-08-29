5a21bcdd33df95e62878aa356d3b794f175712bd
Eduardo Villota <eduardouio7@gmail.com>
2023-08-29 17:48:48 -0500

    archivo de configuracion en tomas

--- src/client/taking-input/src/appConfig.js
+++ src/client/taking-input/src/appConfig.js
@@ -1,11 +1,14 @@
+const idTaking = 303;
+const idTeam = 1742;
 const apiBaseUrl = 'http://localhost:8000';
 
 const appConfig = {
     apiBaseUrl: apiBaseUrl,
-    dataUrl: apiBaseUrl + '/api/mobile/assistant/id_tk/191/id_tm/908/',
+    dataUrl: `${apiBaseUrl}/api/mobile/assistant/id_tk/${idTaking}/id_tm/${idTeam}/`,
     csrfToken: 'aqui_va_el_csrf_token',
-    imgOk: apiBaseUrl + '/static/img/ok.jpg',
-    imgError: apiBaseUrl + '/static/img/error.jpg',
+    imgOk: `${apiBaseUrl}/static/img/ok.jpg`,
+    imgError: `${apiBaseUrl}/static/img/error.jpg`,
+    defaultPicture: `${apiBaseUrl}/static/img/generic_product.png`,
 }
 
 export default appConfig;
