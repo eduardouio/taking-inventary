aaa2b3b3a48a86ff3db4f9fe342f0c6883c6b58a
Eduardo Villota <eduardouio7@gmail.com>
2023-09-02 21:23:53 -0500

    formulario de grupos

--- src/client/taking-input/src/components/FormGroup.vue
+++ src/client/taking-input/src/components/FormGroup.vue
@@ -30,31 +30,72 @@
     			<div class="row mt-1">
     					<button 
 							class="btn btn-primary btn-block" 
-							@click="updateGroup"> 
+							@click="updateTeam"> 
 							<i class="fas fa-users"> </i>Confirmar Grupo</button>
     			</div>
     		</div>
     	</div>
 </template>
 <script>
+import appConfig from ".././appConfig";
+
 export default {
     name: 'FormGroup',
-	emits: ['updateGroup'],
+	emits: ['switchView'],
     props: {
 		team: {
 			type: Object,
 			default: null,
 			required: true,
-		},
-		user:{
+		},user:{
 			type: Object,
 			default: null,
 			required: true,
-		}
+		}, show_view: {
+			type: Object,
+			default: null,
+			required: true,
+		},server_status: {
+			type: Object,
+			default: null,
+			required: true,
+		},have_team: {
+			type: Boolean,
+			default: false,
+			required: true,
+		},
 	},
 	methods: {
-		updateGroup(){
-			this.$emit('updateGroup', this.team)
+		switchView(viewName){
+			console.log('emitimos el evento switchView');
+			this.$emit('switchView', viewName);
+		},updateTeam() {
+		this.show_view.loader = true;
+		this.server_status.response = null;
+		this.server_status.message = null;
+
+		fetch(appConfig.updateTeamURL, {
+			method: 'PUT',
+			headers:appConfig['headers'],
+			body: JSON.stringify(this.team),
+		}).then(response => {
+			if (response.status === 200) {
+			this.server_status.issue_type = 'success';
+			this.server_status.message = 'Completado correctamente';
+			this.server_status.response = response.responseText;
+			console.log(`emitimos el evento switchView ${this.have_team}`);
+			this.switchView('search_form');
+			} else {
+			this.server_status.issue_type = 'error';
+			this.server_status.message = `Servidor Desconectado ${response.responseText}`;
+			this.switchView('group_form');
+			}
+		}).catch(error => {
+			this.show_view.loader = false;
+			this.server_status.issue_type = 'error';
+			this.server_status.message = 'Error al cargar los datos -> ${error}';
+			this.switchView('group_form');
+		});
 		},
 	}
 }
