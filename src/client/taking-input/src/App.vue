e283561b0eba752e14dd8e7a06c8639d51712c59
Eduardo Villota <eduardouio7@gmail.com>
2023-09-04 23:45:19 -0500

    sincronizacion

--- src/client/taking-input/src/App.vue
+++ src/client/taking-input/src/App.vue
@@ -4,56 +4,62 @@
       <loader
         v-if="show_view.loader"
         :server_status="server_status"
-        @changeView="$event => switchView($event)">
+        :show_view="show_view"
+        @switchView="$event => switchView($event)">
       </loader>
       <div>
-        <nav-bar
-          v-if="show_view.loader === false"
-          :taking="taking"
-          :team="team"
-          :user="user"
-          :report="report"
-          @changeView="$event => switchView($event)">
-        </nav-bar>
+      <nav-bar
+        v-if="show_view.loader === false"
+        :taking="taking"
+        :team="team"
+        :user="user"
+        :report="report"
+        @switchView="$event => switchView($event)">
+      </nav-bar>
+      <form-group
+        v-if="!have_team || show_view.group_form"
+        :team="team"
+        :user="user"
+        :have_team="have_team"
+        :show_view="show_view"
+        :server_status="server_status"
+        @switchView="$event => switchView($event)"
+        @updateGroup="$event => updateTeam($event)"
+      >
+      </form-group>
         <search-form 
           v-if="show_view.search_form"
           class="mt-1" :products="products"
           @selectProduct="$event => selectItem($event)">
         </search-form>
         <product-description
-          class="mt-1"
-          v-if="show_view.product_description"
-          :current_item="current_item"
-          :base_url="base_url"
-          @changeView="$event => switchView($event)">
-        </product-description>
-        <form-taking 
-          v-if="show_view.taking_form"
-          :current_item="current_item"
-          :base_url="base_url"
-          :report="report"
-          @changeView="$event => switchView($event)">
-        </form-taking>
-        <report-taking
-          v-if="show_view.report_info"
-          :report="report"
-          :team="team"
-          :user="user"
-          :taking="taking"
-          :base_url="base_url"
-          :server_status="server_status"
-          @removeItem="$event => deteleItemReport($event)"
-          @sendReport="$event => saveReport($event)"
-        >
-        </report-taking>
-        <form-group
-         v-if="!have_team || show_view.group_form"
-         :team="team"
-         :user="user"
-         @updateGroup="$event => updateTeam($event)"
-        >
-        </form-group>
-        <!--<form-product
+        class="mt-1"
+        v-if="show_view.product_description"
+        :current_item="current_item"
+        :base_url="base_url"
+        @switchView="$event => switchView($event)">
+      </product-description>
+      <form-taking 
+      v-if="show_view.taking_form"
+      :current_item="current_item"
+      :base_url="base_url"
+      :report="report"
+      @switchView="$event => switchView($event)">
+    </form-taking>
+    <report-taking
+    v-if="show_view.report_info"
+    :report="report"
+    :team="team"
+    :user="user"
+    :taking="taking"
+    :base_url="base_url"
+    :server_status="server_status"
+    @removeItem="$event => deteleItemReport($event)"
+    @sendReport="$event => saveReport($event)"
+    >
+  </report-taking>
+  <!-- 
+        form-product
           v-if="show_view.product_form"
           :current_item="current_item"
           @updateProduct="$event => updateProduct($event)"
@@ -73,24 +79,24 @@ import "@fortawesome/fontawesome-free/js/all.min.js";
 
 import Loader from "./components/Loader.vue";
 import NavBar from "./components/NavBar.vue";
+import FormGroup from './components/FormGroup.vue';
 import SearchForm from "./components/SearchForm.vue";
 import ProductDescription from "./components/ProductDescription.vue";
-import ReportTaking from "./components/ReportTaking.vue";
 import FormTaking from "./components/FormTaking.vue";
-import FormGroup from './components/FormGroup.vue';
-import FormProduct from './components/FormProduct.vue';
+import ReportTaking from "./components/ReportTaking.vue";
+// import FormProduct from './components/FormProduct.vue';
 
 export default {  
   name: 'App',
   components: {
     Loader,
     NavBar,
+    FormGroup,
     SearchForm,
     ProductDescription,
     FormTaking,
     ReportTaking,
-    FormGroup,
-    FormProduct,
+    // FormProduct,
   },
   data() {
     return {
@@ -107,12 +113,6 @@ export default {
         img_error: appConfig.imgError,
         message: '',
       },
-      current_item: null,
-      report: [],
-      csrf_token: appConfig.csrfToken,
-      have_team: true,
-      report_update: false,
-      show_status_message: true,
       show_view: {
         loader: true,
         search_form: false,
@@ -123,74 +123,113 @@ export default {
         product_description: false,
         status_message: false,
       },
+      current_item: null,
+      report: [],
+      csrf_token: appConfig.csrfToken,
+      have_team: true,
+      report_update: false,
+      show_status_message: true,
       disable_button_send: false,
     }
   },
    methods: {
     getData(){
-      const xhr = new XMLHttpRequest();
-      xhr.open('GET', this.url);
-      xhr.setRequestHeader('Content-Type', 'application/json');
-      xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
-      xhr.onload = () => {
-        if (xhr.status === 200){
-          const data = JSON.parse(xhr.responseText);
-          this.taking = data.taking;
-          this.team = data.team;
-          this.products = data.products;
-          this.user = data.user;
-          this.show_view.loader = false;
-          this.server_status.issue_type = 'success';
-          this.server_status.message = 'Completado correctamente';
-          this.server_status.response = data;
-          this.have_team = Boolean(this.team.warenhouse_assistant);
-      }};
-      xhr.onerror = () => {
+      console.log('cargamos toda la data del usuario');
+      // change to useng fetch api
+      fetch(this.url, {
+        method: 'GET',
+        headers: appConfig['headers']}).then(response => {
+        if (response.status === 200){
+          return response.json();
+        } else {
+          this.show_view.loader = true;
+          this.server_status.issue_type = 'error';
+          this.server_status.message = `Servidor desconectado ${response.statusText}`;
+        }
+      }).then(data => {
+        this.taking = data.taking;
+        this.team = data.team;
+        this.products = data.products;
+        this.user = data.user;
+        this.server_status.issue_type = 'success';
+        this.server_status.message = 'Completado correctamente';
+        this.server_status.response = data;
+        this.have_team = Boolean(this.team.warenhouse_assistant);
+        this.switchView('search_form');
+      }).catch(error => {
         this.show_view.loader = true;
         this.server_status.response = null;
         this.server_status.issue_type = 'error';
-        this.server_status.message = 'Error al cargar los datos -> ' + xhr.statusText;
-      };
-      xhr.send();
+        this.server_status.message = `Servidor desconectado ${error}`;
+      });
     },
     saveReport(){
       // enviar reporte
       this.show_view.loader = true;
       this.server_status.response = null;
-      const xhr_1 = new XMLHttpRequest();
-      xhr_1.open('POST', this.base_url + '/takings/add-report/');
-      xhr_1.setRequestHeader('Content-Type', 'application/json');
-      xhr_1.setRequestHeader('X-CSRFToken', this.csrf_token);
-      xhr_1.onload = () => {
-        this.show_view.report_info = true;
-        this.server_status.response = xhr_1.responseText;
-        if (xhr_1.status === 201) {
-          this.server_status.message = xhr_1.responseText;
-        } else if (xhr_1.status === 400) {
-          this.server_status.message = xhr_1.responseText;
-          this.server_status.issue_type = 'warning';
+
+      // variable para el reporte
+      const dataReport = {
+          force:false,
+          id_team: this.team.id_team,
+          id_taking: this.taking.id_taking,
+          token_team: this.team.token_team,
+          report: this.report.map((item)=>{
+              return {
+                id_product: item.product.id_product,
+                taking_total_boxes: item.taking_total_boxes,
+                taking_total_bottles: item.taking_total_bottles,
+                date_expiry: item.date_expiry,
+                year: item.year,
+                notes: item.notes,
+              }
+          })
         }
-          else{
-          this.server_status.response = null;
+      
+        // Envio de los datos del reporte al back
+      fetch(appConfig['syncUrl'], {
+        method: 'POST',
+        headers: appConfig['headers'],
+        body: JSON.stringify(dataReport),
+      }).then(response => {
+        if (response.status === 201) {
+            const checkSums = {
+              skus: this.report.length,
+              quantity: this.report.reduce((acc, item) => {
+                return acc + (item.taking_total_boxes * item.product.quantity_per_box) + item.taking_total_bottles;
+              }, 0),
+              taking_total_boxes: this.report.reduce((acc, item) =>{
+                return acc + item.taking_total_boxes;
+              },0), 
+              taking_total_bottles: this.report.reduce((acc, item) =>{
+                return acc + item.taking_total_bottles;
+              },0), 
+            }
+            if (checkSums.skus === this.taking.skus && checkSums.quantity === this.taking.quantity && checkSums.taking_total_boxes === this.taking.taking_total_boxes && checkSums.taking_total_bottles === this.taking.taking_total_bottles) {
+              this.server_status.issue_type = 'success';
+              this.server_status.message = 'Completado correctamente';
+              this.server_status.response = response.responseText;
+              this.switchView('search_form');
+            } else {
+              this.server_status.issue_type = 'error';
+              this.server_status.message = `Error en la sincronizacion`;
+              this.server_status.response = response.responseText;
+              this.switchView('search_form');
+            }
+        } else {
           this.server_status.issue_type = 'error';
-          this.server_status.message = 'Error al cargar los datos -> ' + xhr_1.responseText;
+          this.server_status.message = `Servidor desconectado ${response.statusText}`;
+          this.switchView('report_info');
         }
-      };
-      xhr_1.onerror = () => {
+      }).catch(error => {
+        this.show_view.loader = false;
         this.server_status.response = null;
         this.server_status.issue_type = 'error';
-        this.server_status.message = 'Error al cargar los datos -> ' + xhr_1.responseText;
-      };
-      
-      xhr_1.send(JSON.stringify({
-        report: this.report,
-        taking: this.taking,
-        user: this.user,
-        team: this.team,
-      }));
-
+        this.server_status.message = `Servidor desconectado ${error}`;
+      });
     },
     switchView(template_name) {
+      console.log('emitimos el evento switchView ' + template_name);
        this.show_view = {
         loader: false,
         search_form: false,
@@ -202,32 +241,7 @@ export default {
         status_message: false,
         [template_name]: true,
       };
-    },
-    updateTeam(team) {
-      this.team = team;
-      this.show_view.loader = true;
-      this.server_status.response = null;
-      const xhr_team = new XMLHttpRequest();
-      xhr_team.open('POST', this.base_url + '/accounts/update-team/');
-      xhr_team.setRequestHeader('Content-Type', 'application/json');
-      xhr_team.setRequestHeader('X-CSRFToken', this.csrf_token);
-      xhr_team.onload = () => {
-        this.show_view.loader = false;
-        if (xhr_team.status === 200) {
-          this.have_team = true;
-          this.server_status.issue_type = 'success';
-          this.server_status.message = 'Completado correctamente';
-          this.server_status.response = xhr_team.responseText;
-          this.switchView('search_form');
-        } else {
-          this.server_status.issue_type = 'error';
-          this.server_status.message = 'Error al cargar los datos -> ' + xhr_team.responseText;
-          this.switchView('group_form');
-        }
-      };
-        xhr_team.send(JSON.stringify({
-        team: this.team,
-      }));
+      this.have_team = Boolean(this.team.warenhouse_assistant);
     },
     selectItem(product) {
       this.current_item = product;
@@ -243,13 +257,8 @@ export default {
   mounted() {
     // Cargamos datos iniciales de la aplicacion
      this.getData();
-    window.addEventListener("beforeunload", (e) => {
-      e.preventDefault();
-      return e.returnValue = 'Esta seguro de salir?, la información se perderá';
-    });
     }
   }
-
 </script>
 <style>
 .bordered {
